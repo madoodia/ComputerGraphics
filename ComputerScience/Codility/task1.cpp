740c9baf08a3f4a762c35a8865ba2073f5e46458
Reza Aarabi <madoodia@gmail.com>
2023-02-01 11:50:42 -0800

    optimizin assessments

--- ComputerScience/Codility/task1.cpp
+++ ComputerScience/Codility/task1.cpp
@@ -1,17 +1,66 @@
 #include <algorithm>
 #include <vector>
+#include <iostream>
+#include <chrono>
 
-int solution(vector<int>& A)
+int solution1(std::vector<int>& A)
 {
   int N = A.size();
   int result = 0;
 
-  for (int i = 0; i < N - 2; i++)
+  for (int i = 0; i < N; i++)
     for (int j = N - 1; j > i; j--)
       if (A[i] == A[j])
       {
-        result = max(result, abs(i - j));
+        result = std::max(result, abs(i - j));
         return result;
       }
   return result;
+}
+
+int solution2(std::vector<int>& A)
+{
+  int N = A.size();
+  int result = 0;
+  std::vector<int> positions(N, -1);
+  for (int i = 0; i < N; i++)
+  {
+    if (positions[A[i]] != -1)
+    {
+      result = std::max(result, i - positions[A[i]]);
+    }
+    else
+    {
+      positions[A[i]] = i;
+    }
+  }
+  return result;
+}
+
+int main()
+{
+  // std::vector<int> A{2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+  // std::cout << solution(A) << std::endl;
+
+  std::chrono::time_point<std::chrono::system_clock> start, end;
+  int size = 10000;
+  std::vector<int>* A = new std::vector<int>(size);
+
+  for (int i = 0; i < size; i++)
+    (*A)[i] = rand() % 100;
+  {
+    start = std::chrono::system_clock::now();
+    std::cout << solution1(*A) << std::endl;
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::cout << "elapsed time: " << elapsed_seconds.count() << "s" << std::endl;
+  }
+  {
+    start = std::chrono::system_clock::now();
+    std::cout << solution2(*A) << std::endl;
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::cout << "elapsed time: " << elapsed_seconds.count() << "s" << std::endl;
+  }
+  return 0;
 }
--- ComputerScience/Codility/task3.cpp
+++ ComputerScience/Codility/task3.cpp
@@ -2,15 +2,16 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <chrono>
 
 using namespace std;
 
-int solution(vector<int>& A)
+int solution1(vector<int>& A)
 {
   vector<int> result{};
   int max = 1;
 
-  for (unsigned int i = 0; i < A.size()-1; i++)
+  for (unsigned int i = 0; i < A.size() - 1; i++)
   {
     for (unsigned int j = i; j < A.size(); j++)
     {
@@ -32,10 +33,66 @@ int solution(vector<int>& A)
   return max;
 }
 
+int solution2(vector<int>& A)
+{
+  vector<int> result{};
+  int max = 1;
+
+  for (unsigned int i = 0; i < A.size(); i++)
+  {
+    if (A[i] >= 0)
+    {
+      result.push_back(A[i]);
+      i++;
+      while (i < A.size() && A[i] <= 0)
+      {
+        result.push_back(A[i]);
+        i++;
+      }
+    }
+    else
+    {
+      result.push_back(A[i]);
+      i++;
+      while (i < A.size() && A[i] >= 0)
+      {
+        result.push_back(A[i]);
+        i++;
+      }
+    }
+    if (result.size() > max)
+      max = result.size();
+    result.clear();
+  }
+  return max;
+}
+
 int main()
 {
   // vector<int> A{5, 4, -3, 2, 0, 1, -1, 0, 2, -3, 4, -5};
-  vector<int> A{1, 2, 3};
-  cout << solution(A) << endl;
+  // vector<int> A{1, 2, 3};
+
+  std::chrono::time_point<std::chrono::system_clock> start, end;
+  int size = 10000;
+  std::vector<int>* A = new std::vector<int>(size);
+
+  for (int i = 0; i < size; i++)
+    // fill the array with random signed number in range [-100, 100]
+    (*A)[i] = rand() % 201 - 100;
+  {
+    start = std::chrono::system_clock::now();
+    std::cout << solution1(*A) << std::endl;
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::cout << "elapsed time: " << elapsed_seconds.count() << "s" << std::endl;
+  }
+  {
+    start = std::chrono::system_clock::now();
+    std::cout << solution2(*A) << std::endl;
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::cout << "elapsed time: " << elapsed_seconds.count() << "s" << std::endl;
+  }
+
   return 0;
 }
